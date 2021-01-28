6d1c12bf02e2e3ae4834092f257c3d35229dd495
baalajimaestro <me@baalajimaestro.me>
2021-01-28 13:03:29 +0530

    sm8250-common: Optimise dex flags for a faster boot
    * Used multiple threads and speed profile to hasten the first boot
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: I2cce5ddf7d50308511e81436fcac613b2c6537bf

--- kona.mk
+++ kona.mk
@@ -179,6 +179,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
